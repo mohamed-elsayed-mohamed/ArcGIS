1dd3b0ad8bcf498f62d07d6ae7676afebdc6342f
Mohamed Elsayed <mohamed_elsayed.1995@yahoo.com>
2021-10-05 20:35:43 +0200

    Initial commit

---
+++ 001_display_map/src/main/java/com/alpha/arcgis/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.alpha.arcgis
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alpha.arcgis.databinding.ActivityMainBinding
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.BasemapStyle
+import com.esri.arcgisruntime.mapping.Viewpoint
+import com.esri.arcgisruntime.mapping.view.MapView
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mapview: MapView by lazy { binding.map }
+
+    private var defaultViewPoint: Viewpoint = Viewpoint(30.052697, 31.198192, 72000.0)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupMap()
+    }
+
+    private fun setupMap(){
+        ArcGISRuntimeEnvironment.setApiKey(BuildConfig.API_KEY)
+        mapview.map = ArcGISMap(BasemapStyle.ARCGIS_TOPOGRAPHIC)
+        mapview.setViewpoint(defaultViewPoint)
+    }
+
+    override fun onPause() {
+        mapview.pause()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapview.resume()
+    }
+
+    override fun onDestroy() {
+        mapview.dispose()
+        super.onDestroy()
+    }
+}
---
+++ 002_add_point_line_polygon/src/main/java/com/alpha/a002_add_point_line_polygon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alpha.a002_add_point_line_polygon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
