da1deb2fc969c581c68a3d8e183410c0fe18ee83
Mohamed.Hasanen <m.elsayed.m.1995@gmail.com>
2021-11-08 10:38:47 +0200

    Display mmpk offline map

---
+++ 010_display_mmpk_map/src/main/java/com/alpha/a010_display_mmpk_map/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alpha.a010_display_mmpk_map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
